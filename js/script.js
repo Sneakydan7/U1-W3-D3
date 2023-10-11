bc83e5a8a0bfdce05bceed21d81f991fe3862069
daniele cagnoni <daniele.010@hotmail.it>
2023-10-11 16:56:16 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,32 @@
+window.onload = () => {
+  const submitButton = document.getElementById("submit");
+  const inputLoc = document.getElementById("inputBox");
+  const ul = document.getElementById("toDoList");
+
+  submitButton.onclick = function () {
+    const inputValue = inputLoc.value;
+
+    const newLi = document.createElement("li");
+    newLi.innerText = inputValue;
+    newLi.classList = "newLi";
+
+    const newX = document.createElement("span");
+    newX.innerText = "X";
+    newX.className = "close";
+
+    newLi.appendChild(newX);
+
+    ul.appendChild(newLi);
+
+    newX.onclick = function (e) {
+      const toRemove = e.target;
+      toRemove.parentNode.remove();
+    };
+    const list = document.querySelector("ul");
+
+    const checked = function (e) {
+      e.target.classList.toggle("strikethrough");
+    };
+    list.addEventListener("click", checked);
+  };
+};
